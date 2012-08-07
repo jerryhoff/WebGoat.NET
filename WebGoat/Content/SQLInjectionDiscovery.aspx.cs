7d556691e433bbb3e48c8124b6321a331057bbba
raybeorn <raybeorn@gmail.com>
2012-08-07 11:40:40 -0500

    Added note if customer does not exist

--- WebGoat/Content/SQLInjectionDiscovery.aspx.cs
+++ WebGoat/Content/SQLInjectionDiscovery.aspx.cs
@@ -27,7 +27,7 @@ namespace OWASP.WebGoat.NET
                 string name = txtID.Text.Substring(0, 3);
                 string output = du.GetEmailByCustomerNumber(name);
 
-                lblOutput.Text = output;
+                lblOutput.Text = String.IsNullOrEmpty(output) ? "Customer Number does not exist" : output;
             }
             catch (Exception ex)
             {
